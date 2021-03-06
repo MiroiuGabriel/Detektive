b9758d53606d43aa4e7f419f68509d722e047e6f
Gabriel Miroiu <miroiugabriel1231@gmail.com>
2021-03-06 23:26:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const styleCanvas = (canvas, imageLocation, color = '#fff') => {
+	const ctx = canvas.getContext('2d');
+
+	//Resizing
+	let background = new Image();
+	background.src = imageLocation;
+	canvas.height = background.height;
+	canvas.width = background.width;
+
+	background.onload = function () {
+		ctx.drawImage(background, 0, 0);
+	};
+	//variables
+	let painting = false;
+
+	function startPosition(e) {
+		painting = true;
+		draw(e);
+	}
+	function finishedPosition() {
+		painting = false;
+		ctx.beginPath();
+	}
+	function draw(e) {
+		if (!painting) return;
+		ctx.lineWidth = 2;
+		ctx.lineCap = 'round';
+		ctx.lineTo(e.layerX, e.layerY);
+		ctx.strokeStyle = color;
+		ctx.stroke();
+		ctx.moveTo(e.layerX, e.layerY);
+	}
+
+	canvas.addEventListener('mousedown', startPosition);
+	window.addEventListener('mouseup', finishedPosition);
+	canvas.addEventListener('mousemove', draw);
+};
+
+window.addEventListener('load', () => {
+	const lab = document.querySelector('#lab');
+	const crowd = document.querySelector('#crowd');
+	const train = document.querySelector('#train');
+	const impostor = document.querySelector('#impostor');
+	const dinner = document.querySelector('#dinner');
+	const find = document.querySelector('#find');
+	const poster = document.querySelector('#poster');
+	const code = document.querySelector('#code');
+	const suitcase = document.querySelector('#suitcase');
+	const safe = document.querySelector('#safe');
+	const whiteboard = document.querySelector('#whiteboard');
+	const bedroom = document.querySelector('#bedroom');
+	const puzzle = document.querySelector('#puzzle');
+	const trains = document.querySelector('#trains');
+	const found = document.querySelector('#found');
+
+	styleCanvas(lab, './images/image--003.jpg', '#000');
+	styleCanvas(crowd, './images/image--004.jpg', '#000');
+	styleCanvas(train, './images/image--005.jpg', '#000');
+	styleCanvas(impostor, './images/image--006.jpg');
+	styleCanvas(dinner, './images/image--008.jpg');
+	styleCanvas(find, './images/image--009.jpg');
+	styleCanvas(poster, './images/image--010.jpg');
+	styleCanvas(code, './images/image--011.jpg', '#000');
+	styleCanvas(suitcase, './images/image--012.jpg', '#000');
+	styleCanvas(safe, './images/image--013.jpg', '#000');
+	styleCanvas(whiteboard, './images/image--014.jpg', '#000');
+	styleCanvas(bedroom, './images/image--015.jpg', '#000');
+	styleCanvas(puzzle, './images/image--016.jpg', '#000');
+	styleCanvas(trains, './images/image--017.jpg', '#000');
+	styleCanvas(found, './images/image--018.jpg', '#000');
+});
+const draggables = document.querySelectorAll('.answer');
+const containers = document.querySelectorAll('.drag');
+
+draggables.forEach(draggable => {
+	draggable.addEventListener('dragstart', () => {
+		draggable.classList.add('dragging');
+	});
+	draggable.addEventListener('dragend', () => {
+		draggable.classList.remove('dragging');
+	});
+});
+containers.forEach(container => {
+	container.addEventListener('dragover', e => {
+		if (container.classList.contains('drag-into-div')) {
+			if (container.childElementCount >= 6) {
+				return;
+			}
+		}
+		container.classList.add('drag-outline');
+		const draggable = document.querySelector('.dragging');
+		container.appendChild(draggable);
+	});
+	container.addEventListener('dragleave', () => {
+		container.classList.remove('drag-outline');
+	});
+});
+const downloadBtn = document.querySelector('.download-button');
+downloadBtn.href = 'https://drive.google.com/u/0/uc?id=13nUhxM8xrkanlva9xfTpbisOD-pfvJzw&export=download';
+downloadBtn.download = 'answers';
